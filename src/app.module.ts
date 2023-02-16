47d2f92436aa12a27a2df5ef4e096b25ed3a945a
peyman shahbazi <peymanshahbazi@peymans-MacBook-Pro.local>
2023-02-16 19:16:32 +0330

    remove useless files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProvincesController } from './http/controllers/provinces.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProvincesController],
+  controllers: [ProvincesController],
   providers: [AppService],
 })
 export class AppModule {}
